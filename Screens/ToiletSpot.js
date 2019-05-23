cdd0aab2c436315a0c2558e38cc94ae6b8233715
delicioushwan <tmdghks0217@gmail.com>
2019-05-23 20:21:04 +0900

    put distance each toilet

--- Screens/ToiletSpot.js
+++ Screens/ToiletSpot.js
@@ -6,6 +6,22 @@ import { Ionicons } from '@expo/vector-icons';
 const ToiletSpot = (props) => {
     const latitude = Number(props.toiletLocation.latitude)
     const longitude = Number(props.toiletLocation.longitude)
+    const CalulateToMeters = (lat1, lon1, lat2, lon2) => {
+        var R = 6378.137;
+        var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
+        var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+        Math.sin(dLon/2) * Math.sin(dLon/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return d * 1000 ;
+    }
+    let lat1 = props.currentLocation.latitude
+    let lon1 = props.currentLocation.longitude
+    let lat2 = props.toiletLocation.latitude
+    let lon2 = props.toiletLocation.longitude
+    let distance = Math.floor(CalulateToMeters(lat1,lon1,lat2,lon2))
     return(
         <MapView.Marker
             coordinate={
@@ -15,7 +31,7 @@ const ToiletSpot = (props) => {
                 }
             }
             title="똥통"
-            description={props.toiletLocation.description}
+            description={''+distance + 'm'}
             onCalloutPress={() => {
                 props.navigation.navigate("AddComment",{"infos" : props})}}
         >
